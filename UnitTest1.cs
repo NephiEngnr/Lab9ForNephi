5dc06c92cb395f88c7c6d2d51a95f3284050be6d
unknown <nephi.allen@ENGINEERING.SNOW>
2025-10-21 17:10:04 -0600

    Flow chart done

--- UnitTest1.cs
+++ UnitTest1.cs
@@ -16,14 +16,28 @@ string noun = "Please enter a new noun: (name an animal)\n";
 
 Console.Write(adj);
 string adj1 = Console.ReadLine();
-
-Console.Write(verb);
+Console.Write(verb+" (past tense) \n");
 string verb1 = Console.ReadLine();
-
 Console.Write(noun);
 string noun1 = Console.ReadLine();
+Console.Write(adj);
+string adj2 = Console.ReadLine();
+Console.Write(verb + " (past tense) \n");
+string verb2 = Console.ReadLine();
+Console.Write(verb + " (past tense) \n");
+string verb3 = Console.ReadLine();
+Console.Write(noun);
+string noun2 = Console.ReadLine();
 
-string story = "There was a " + adj1 + " " + noun1 + ". This " + noun1 + " tried to " + verb1 + " its pet turtle. \nIt did not go that well.\n";
+Console.Write(".\n");
+
+string story = $@"Once upon a time, there was a(n) {adj1} boy who {verb1} sheep.
+He wanted some excitement so he pretended that there was a(n) {adj2} {noun1} attacking his ^plural-noun.
+When the townspeople came, he {verb2} them. 
+He had so much fun that he tried it again the following day and {verb3} at the townspeople when they came to help.
+The next day, a(n) {noun2} really did {verb3}";//his sheep and he {^past-tense-verb} and {^past-tense-verb} to call for help,
+//but no one believed him and he lost his {^noun} .";
+
 Console.WriteLine(story);
 Console.WriteLine("I hope you enjoyed this! Have a great day. 😇");
 
